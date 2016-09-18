e04a6c7ba201e990a5a581eae9289643f2da279e
Dean Perry <dean@voupe.com>
2016-09-18 18:08:03 +0100

    rails updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,10 +42,13 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
