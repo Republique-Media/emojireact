b6e0c242529f8c3290eab4d86fc44a529657c8ae
Dean Perry <dean@voupe.com>
2018-03-21 14:43:46 +0000

    rails 5.1.5 changes

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,15 @@ Bundler.require(*Rails.groups)
 
 module EmojiReact
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+end
+
+Raven.configure do |config|
+  config.dsn = 'https://ab0b5da23d0e420880534a7c03bf4872:685f8494c1314f0bb1cc9653be84ef6a@sentry.io/300715'
 end
